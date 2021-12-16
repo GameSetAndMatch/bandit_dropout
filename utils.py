2a8da7839e1cebb2090d14397299b4d93d8a17a4
OlivierBelhumeur <belhumeurolivier@gmail.com>
2021-12-16 18:07:52 -0500

    ajout save_to_pkl

--- utils.py
+++ utils.py
@@ -1,9 +1,17 @@
+from typing import List
 import numpy
 import torch
 import random
-
+import os
+import pickle as pk 
 
 def set_random_seed(seed):
     torch.manual_seed(seed)
     numpy.random.seed(seed)
     random.seed(seed)
+    
+def save_to_pkl(history:List, file_name:str, directory_name:str = "results/history"):
+    if not os.path.exists(directory_name):
+        os.makedirs(directory_name)
+    with open(f'results/history/{file_name}.pkl', 'wb') as f:
+        pk.dump(history, f)

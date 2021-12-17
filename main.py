6a15b4ffd7f115a18119c657c79e983c787c9ad3
OlivierBelhumeur <belhumeurolivier@gmail.com>
2021-12-16 20:59:51 -0500

    my main vide

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from random import seed
 import linUcb
 import RandomDropout
 import RegularDropout
@@ -9,17 +8,4 @@ import boltzmannDropout
 
 
 if __name__ == '__main__':
-
-   linUcb.run_experience(exp_name="Acc_Increase_linUcb_batch",reward_type="accuracy_increase",nombre_epoch=12,nombre_entrainement=10,seed=42)
-   linUcb.run_experience(reward_type="Acc_linUcb_batch",reward_type="accuracy",nombre_epoch=12,nombre_entrainement=10,seed=42) 
-   linUcb.run_experience(exp_name="loss_Increase_linUcb_batch",reward_type="loss_increase",nombre_epoch=12,nombre_entrainement=10,seed=42)
-   linUcb.run_experience(reward_type="loss_linUcb_batch",reward_type="loss",nombre_epoch=12,nombre_entrainement=10,seed=42)
-   linUcb.run_experience(exp_name="Acc_Increase_linUcb_epoch",reward_type="accuracy_increase",per_batch=False,nombre_epoch=12,nombre_entrainement=10,seed=42)
-   linUcb.run_experience(reward_type="Acc_linUcb_epoch",reward_type="accuracy",per_batch=False,nombre_epoch=12,nombre_entrainement=10,seed=42) 
-   linUcb.run_experience(exp_name="loss_Increase_linUcb_epoch",reward_type="loss_increase",per_batch=False,nombre_epoch=12,nombre_entrainement=10,seed=42)
-   linUcb.run_experience(reward_type="loss_linUcb_epoch",reward_type="loss",per_batch=False,nombre_epoch=12,nombre_entrainement=10,seed=42)
-
-   RandomDropout.run_experience(nombre_entrainement=10,nb_epoch=12,exp_name="random_dropout",seed=42)
-   RegularDropout.run_experience(nombre_entrainement=10,nombre_epoch=12,p=0.5,exp_name="Reg_dropout0_5")
-   RegularDropout.run_experience(nombre_entrainement=10,nombre_epoch=12,p=0.5,exp_name="Reg_dropout0_2")
-
+    pass
